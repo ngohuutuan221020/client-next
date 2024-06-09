d3092516e1b7191bb1836f82f6a91e497069b9d7
Ngo Huu Tuan <ngohuutuan221020@gmail.com>
2024-06-09 22:47:28 +0700

    update

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Manga from "./Manga/page";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center ">
-      <Manga />
-    </div>
-  );
-}
