51bbed1f1e2b26f53a82f324cccc58a12b9da73d
Ngo Huu Tuan <ngohuutuan221020@gmail.com>
2024-06-14 23:31:28 +0700

    updateAPI

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -1,41 +1,49 @@
-const PageAPI = async () => {
+import React from "react";
+
+export const getServerSideProps = async () => {
   const res = await fetch("https://server-manga-five.vercel.app/api", {
     method: "GET",
   });
 
   const data = await res.json();
 
+  return {
+    props: {
+      data: data.data,
+    },
+  };
+};
+
+const PageAPI = ({ data }) => {
   return (
     <>
-      {data.data
+      {data
         .sort((a, b) => a.name.localeCompare(b.name))
-        .map((data) => {
-          let url = `https://drive.google.com/file/d/${data.id}/preview`;
+        .map((item) => {
+          let url = `https://drive.google.com/file/d/${item.id}/preview`;
           return (
-            <>
-              <div
-                key={data.id}
+            <div
+              key={item.id}
+              style={{
+                height: "100vh",
+                width: "100%",
+              }}
+            >
+              <h4
                 style={{
-                  height: "100vh",
-                  width: "100%",
+                  color: "blue",
+                  textAlign: "center",
                 }}
               >
-                <h4
-                  style={{
-                    color: "blue",
-                    textAlign: "center",
-                  }}
-                >
-                  {data.name}
-                </h4>
-                <iframe
-                  src={url}
-                  width="100%"
-                  height="100%"
-                  allow="autoplay"
-                ></iframe>
-              </div>
-            </>
+                {item.name}
+              </h4>
+              <iframe
+                src={url}
+                width="100%"
+                height="100%"
+                allow="autoplay"
+              ></iframe>
+            </div>
           );
         })}
     </>
