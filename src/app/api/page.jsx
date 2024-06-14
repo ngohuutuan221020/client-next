42462dd03ab20da4ffab2df60283a79b2bee209a
Ngo Huu Tuan <ngohuutuan221020@gmail.com>
2024-06-14 23:39:21 +0700

    update

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -1,49 +1,43 @@
-import React from "react";
-
-export const getServerSideProps = async () => {
-  const res = await fetch("https://server-manga-five.vercel.app/api", {
-    method: "GET",
-  });
+const PageAPI = async () => {
+  const res = await fetch(
+    "https://server-manga-five.vercel.app/api",
+    { method: "GET" },
+    { next: { revalidate: 0 } }
+  );
 
   const data = await res.json();
 
-  return {
-    props: {
-      data: data.data,
-    },
-  };
-};
-
-const PageAPI = ({ data }) => {
   return (
     <>
-      {data
+      {data.data
         .sort((a, b) => a.name.localeCompare(b.name))
-        .map((item) => {
-          let url = `https://drive.google.com/file/d/${item.id}/preview`;
+        .map((data) => {
+          let url = `https://drive.google.com/file/d/${data.id}/preview`;
           return (
-            <div
-              key={item.id}
-              style={{
-                height: "100vh",
-                width: "100%",
-              }}
-            >
-              <h4
+            <>
+              <div
+                key={data.id}
                 style={{
-                  color: "blue",
-                  textAlign: "center",
+                  height: "100vh",
+                  width: "100%",
                 }}
               >
-                {item.name}
-              </h4>
-              <iframe
-                src={url}
-                width="100%"
-                height="100%"
-                allow="autoplay"
-              ></iframe>
-            </div>
+                <h4
+                  style={{
+                    color: "blue",
+                    textAlign: "center",
+                  }}
+                >
+                  {data.name}
+                </h4>
+                <iframe
+                  src={url}
+                  width="100%"
+                  height="100%"
+                  allow="autoplay"
+                ></iframe>
+              </div>
+            </>
           );
         })}
     </>
